800c745f377093a1718b92fd83d55e227537ad16
ATstudio <ATstudiorussia@gmail.com>
2022-02-05 16:35:40 +0300

    Here we go again...

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Jumper_again\venv
+set VIRTUAL_ENV=D:\pythonProject6\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
